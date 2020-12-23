e1b16b31aada21bf82a05211ee839fd18afcd134
davidauza.engineer <davidauza01@gmail.com>
2020-12-23 16:30:12 -0500

    Add initialize lifecycle method

--- src/controllers/card_controller.js
+++ src/controllers/card_controller.js
@@ -3,13 +3,39 @@ import { Controller } from 'stimulus';
 export default class extends Controller {
   static targets = ['name'];
 
+  initialize() {
+    const name = document.querySelector('.card-title').getAttribute('data-name');
+
+    if (this.titleName === null) {
+      this.readName(name);
+    } else {
+      this.readName(this.titleName);
+    }
+  }
+
+  get titleName() {
+    return this.data.get('name');
+  }
+
+  set titleName(value) {
+    this.data.set('name', value);
+  }
+
   greet(event) {
     event.preventDefault();
     const name = this.nameTarget.value;
     console.log(`Hello, ${name}`);
+    this.titleName = name;
+    
+    this.readName(this.titleName);
   }
 
   connect() {
     console.log(`I'm the ${this.element.nodeName}`, this.element);
+  }
+
+  readName(name) {
+    const cardName = document.querySelector('.card-title');
+    cardName.textContent = name;
   }
 }
